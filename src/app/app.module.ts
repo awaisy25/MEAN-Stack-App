9c74fe80db2e78bf65f808c85b5cdd5d8fccd733
Awais Hussain <awais.t.hussain@gmail.com>
2019-10-24 21:23:16 -0400

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// need to tell typescript where other file is
+import {PostCreateComponent} from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Angular knows the post component
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+// import component before using it
+@Component({
+  // html tag to be put in html file
+  selector: 'app-post-create',
+  // set the html filein template url
+  templateUrl: './post-create-component.html'
+})
+export class PostCreateComponent {
+
+}
